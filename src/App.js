a6e1b71f38ba0da880f2ce15c1ce6801951f5201
Anji reddy <>
2021-04-06 20:02:22 +0530

    gsdgdsg

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Login from './components/Login';
-import Signup from './components/Singup';
+import Signup from './components2/Signup';
 import Feed from './components/Feed';
+import Auth from './components2/Auth';
+import PlayGround from './components2/PlayGround';
+import Html from './components2/Html';
+import './Global.css';
 
 
 function App() {
@@ -22,7 +26,7 @@ function App() {
       <div className="row">
         <div className="col-lg-12">
         <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Html} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/feed" component={Feed} />
